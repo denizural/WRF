2e38bb4363329dff0e395bc93a5c93d945c5bc23
Deniz Ural <denizural86@gmail.com>
2023-04-10 09:30:44 +0000

    adds missing unistd.h and new header gribhdr2file.h

--- external/io_grib1/MEL_grib1/gribhdr2file.c
+++ external/io_grib1/MEL_grib1/gribhdr2file.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "dprints.h"		/* for dprints */
 #include "gribfuncs.h"		/* prototypes */
+#include "gribhdr2file.h"
 /*
 *
 ************************************************************************
