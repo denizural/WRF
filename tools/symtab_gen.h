886c3916977605105f53ad0351b930295ee7a7d7
Deniz Ural <denizural86@gmail.com>
2023-04-11 16:05:20 +0000

    fixes symtab_gen.h

--- tools/symtab_gen.h
+++ tools/symtab_gen.h
@@ -1,10 +1,20 @@
 #ifndef SYMTAB_GEN_H
 #define SYMTAB_GEN_H
-char *symget
+#include "sym.h"
+sym_nodeptr newnode();
+char** nodename(sym_nodeptr p);
+sym_nodeptr* nodenext(sym_nodeptr p);
+
+char* symget
 (
-    char *name, char *(*newnode), char **(*nodename),
-    char **(*nodenext), char *symtab[], int flag
+    char *name,
+    sym_nodeptr (*newnode)(),
+    char **(*nodename)(sym_nodeptr p),
+    sym_nodeptr* (*nodenext)(sym_nodeptr p),
+    char *symtab[],
+    int flag
 );
+
 int hash(char *name);
 int create_ht(char ***p);
 int sym_traverse(char *ht[], char **(*nodenext)(), void (*f)());
